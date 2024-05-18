e6a9f3c69aa6ffc364dd8c6915606bb1718af458
sb <jiutto@yandex.ru>
2024-05-18 03:24:22 +0300

    Updates

--- Lesson 6/HW-11.py
+++ Lesson 6/HW-11.py
@@ -2,8 +2,8 @@ import requests
 import json
 from typing import Dict
 
-
 class LegalAPI:
+
     BASE_URL = "https://api.sirotinsky.com"
 
     def __init__(self, token: str):
@@ -15,9 +15,13 @@ class LegalAPI:
 
     def _get(self, endpoint: str, id: str) -> Dict:
         url = f"{self.BASE_URL}/{endpoint}/{id}"
-        response = requests.get(url, headers=self.headers)
-        response.raise_for_status()
-        return response.json()
+        try:
+            response = requests.get(url, headers=self.headers)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"Ошибка при отправке запроса: {e}")
+            return {}
 
     def get_organisation(self, efrsb_id: int) -> Dict:
         return self._get("efrsb/organisation", str(efrsb_id))
@@ -28,18 +32,21 @@ class LegalAPI:
     def get_trader(self, ogrn: str) -> Dict:
         return self._get("efrsb/trader", ogrn)
 
-
 if __name__ == "__main__":
+
     api = LegalAPI('4123saedfasedfsadf4324234f223ddf23')
 
     organisation_id = 123456  # Замените на правильный идентификатор ЕФРСБ
     data = api.get_organisation(organisation_id)
-    print(json.dumps(data, indent=4))
+    if data:
+        print(json.dumps(data, indent=4))
 
     inn = '1234567890'  # Замените на правильный ИНН
     data = api.get_person(inn)
-    print(json.dumps(data, indent=4))
+    if data:
+        print(json.dumps(data, indent=4))
 
     ogrn = '1234567890123'  # Замените на правильный ОГРН
     data = api.get_trader(ogrn)
-    print(json.dumps(data, indent=4))
+    if data:
+        print(json.dumps(data, indent=4))
