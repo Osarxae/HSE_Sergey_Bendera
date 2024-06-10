9448c9590564eb3bc950b35173cf58452bf72f8b
sb <jiutto@yandex.ru>
2024-06-10 21:35:29 +0300

    Updates

--- Lesson 7/HW-13ver2.py
+++ Lesson 7/HW-13ver2.py
@@ -2,6 +2,7 @@ import requests
 from datetime import datetime
 from bs4 import BeautifulSoup
 import json
+import pickle
 
 
 class ParserCBRF:
@@ -31,16 +32,35 @@ class ParserCBRF:
         self.data = self.__parse_html(url)
         return self.data
 
+    def save_to_json(self, file_path):
+        """Сохранение данных в JSON-файл"""
+        with open(file_path, 'w', encoding='utf-8') as f:
+            json.dump(self.data, f, indent=4, ensure_ascii=False, sort_keys=True)
+        print(f"Данные сохранены в {file_path}")
+
+    def save_to_pickle(self, file_path):
+        """Сохранение данных в Pickle-файл"""
+        with open(file_path, 'wb') as f:
+            pickle.dump(self.data, f)
+        print(f"Данные сохранены в {file_path}")
+
+    def load_from_pickle(self, file_path):
+        """Загрузка данных из Pickle-файла"""
+        with open(file_path, 'rb') as f:
+            self.data = pickle.load(f)
+        print(f"Данные загружены из {file_path}")
+        return self.data
+
 
 def main():
     parser = ParserCBRF()
     try:
         data = parser.start()
         if data:
-            file_path = f"CBRF_{datetime.now().strftime('%Y-%m-%d')}.json"
-            with open(file_path, 'w', encoding='utf-8') as f:
-                json.dump(data, f, indent=4, ensure_ascii=False, sort_keys=True)
-            print(f"Данные сохранены в {file_path}")
+            json_file_path = f"CBRF_{datetime.now().strftime('%Y-%m-%d')}.json"
+            pickle_file_path = f"CBRF_{datetime.now().strftime('%Y-%m-%d')}.pkl"
+            parser.save_to_json(json_file_path)
+            parser.save_to_pickle(pickle_file_path)
         else:
             print("Не удалось получить данные.")
     except requests.exceptions.RequestException as e:
